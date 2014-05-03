0baea50c8595c7c2859e72cc3993c2c4c7809a7e
Marvin Blum <marvin@marvinblum.de>
2014-05-03 20:52:14 +0200

    Added logo to main menu.

--- src/states/MainMenu.cpp
+++ src/states/MainMenu.cpp
@@ -17,10 +17,12 @@ void MainMenu::load(){
 	textures.push_back(std::shared_ptr<bb::Texture>(new bb::Texture(GL_TEXTURE_2D)));
 	textures.push_back(std::shared_ptr<bb::Texture>(new bb::Texture(GL_TEXTURE_2D)));
 	textures.push_back(std::shared_ptr<bb::Texture>(new bb::Texture(GL_TEXTURE_2D)));
+	textures.push_back(std::shared_ptr<bb::Texture>(new bb::Texture(GL_TEXTURE_2D)));
 
 	textures[0]->loadTGA("res/textures/exit.tga");
 	textures[1]->loadTGA("res/textures/settings.tga");
 	textures[2]->loadTGA("res/textures/start.tga");
+	textures[3]->loadTGA("res/textures/logo.tga");
 
 	//// entities
 	auto exit = std::shared_ptr<bb::Entity>(new bb::Entity());
@@ -38,6 +40,11 @@ void MainMenu::load(){
 	start->addComponent("Position", std::shared_ptr<bb::Position2D>(new bb::Position2D(bb::vec2(wndSize[0]-300, 40), textures[2]->getSize())));
 	start->addComponent("Object2D", std::shared_ptr<bb::Object2D>(new bb::Object2D()));
 
+	auto logo = std::shared_ptr<bb::Entity>(new bb::Entity());
+	logo->addComponent("Texture", textures[3]);
+	logo->addComponent("Position", std::shared_ptr<bb::Position2D>(new bb::Position2D(bb::vec2(40, wndSize[1]-textures[3]->height()-40), textures[3]->getSize())));
+	logo->addComponent("Object2D", std::shared_ptr<bb::Object2D>(new bb::Object2D()));
+
 	//// systems
 	renderer = std::unique_ptr<Renderer>(new Renderer(std::unique_ptr<bb::Shader>(new bb::Shader("res/shader/renderer.vertex", "res/shader/renderer.fragment")), camera));
 	renderer->shader->bindAttrib("vertex0");
@@ -46,6 +53,7 @@ void MainMenu::load(){
 	renderer->addEntity(exit);
 	renderer->addEntity(settings);
 	renderer->addEntity(start);
+	renderer->addEntity(logo);
 
 	hasStarted = true;
 }
